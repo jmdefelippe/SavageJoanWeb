beafe88c0563ca825eade4885872bc09c95aa462
Joan Manuel Defelippe <jmdefelippe@gmail.com>
2021-06-01 17:13:49 -0300

    working on header

--- src/views/components/layout/Header.js
+++ src/views/components/layout/Header.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation, Link } from "react-router-dom";
 import { styles } from './Styles';
 
 const Header = () => {
-    const { header, headerLinkActive, headerLinkInactive } = styles;
+    const { header, headerLinkActive, headerLinkInactive, headerMenu } = styles;
 
     const links = [
         { text: "Inicio", url: "/" },
@@ -20,17 +20,38 @@ const Header = () => {
     ];
 
     const location = useLocation();
+/*
+    const [menu, showMenu] = useState(true);
+    const onClickShowMenu = () => showMenu(!menu);
+*/
+
+    const onClickShowMenu = () => {
+       // menu.classList.toggle('hidden');
+       console.log("toggle...");
+    }
 
     return (
         <div className={header}>
-            {links.map((link) => (
-                <Link key={link.text} to={link.url} my={4} mx={8}>
-                    <a className={location.pathname === link.url
-                        ? `${headerLinkActive}`
-                        : `${headerLinkInactive}`
-                    }>{link.text}</a>
-                </Link>
-            ))}
+            <div className="block lg:hidden">
+                <button
+                    id='button'
+                    className="flex items-center px-3 py-2 mr-10 border rounded text-white border-white hover:text-black hover:border-black"
+                    onClick={() => onClickShowMenu() }
+                >
+                    <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+                </button>
+            </div>
+            
+            <div id='menu' className={headerMenu}>
+                {links.map((link) => (
+                    <Link key={link.text} to={link.url} my={4} mx={8}>
+                        <a className={location.pathname === link.url
+                            ? `${headerLinkActive}`
+                            : `${headerLinkInactive}`
+                        }>{link.text}</a>
+                    </Link>
+                ))}
+            </div>
         </div>
     );
 }
--- src/views/components/layout/Styles.js
+++ src/views/components/layout/Styles.js
@@ -1,7 +1,9 @@
 export const styles = {
     header: `flex flex-wrap px-4 py-4 bg-red-600`,
-    headerLinkActive: `text-white text-sm font-bold px-10 underline`,
-    headerLinkInactive: `text-white text-sm font-bold px-10`,
+    headerLinkActive: `text-white text-sm font-bold px-10 underline block`,
+    headerLinkInactive: `text-white text-sm font-bold px-10 block`,
+
+    headerMenu: `w-full block flex-grow lg:flex lg:items-center lg:w-auto text-center`,
 
     footer: `grid grid-cols-1 text-center py-4 bg-red-600`,
     textFooter: `text-white text-sm`
