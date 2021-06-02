2aae4ef1b2b37db5db3a82fd2f15a636d96096f5
Joan Manuel Defelippe <jmdefelippe@gmail.com>
2021-06-02 19:44:02 -0300

    toggle links header

--- src/views/components/layout/Header.js
+++ src/views/components/layout/Header.js
@@ -3,32 +3,28 @@ import { useLocation, Link } from "react-router-dom";
 import { styles } from './Styles';
 
 const Header = () => {
-    const { header, headerLinkActive, headerLinkInactive, headerMenu } = styles;
+    const { header, headerLinkActive, headerLinkInactive, headerMenuVisible, headerMenuHidden } = styles;
 
     const links = [
         { text: "Inicio", url: "/" },
         { text: "Discografía", url: "/discografia" },
         { text: "Descargas", url: "/descargas" },
-        
+/*        
         { text: "Screen Tabs", url: "/screenTabs" },
         { text: "Tutoriales", url: "/tutoriales" },
         { text: "Backing Tracks", url: "/backingTracks" },
-        
+*/      
         { text: "Videos", url: "/videos" },        
         { text: "Fotos", url: "/fotos" },
         { text: "Contacto", url: "/contacto" },
     ];
 
     const location = useLocation();
-/*
-    const [menu, showMenu] = useState(true);
-    const onClickShowMenu = () => showMenu(!menu);
-*/
 
-    const onClickShowMenu = () => {
-       // menu.classList.toggle('hidden');
-       console.log("toggle...");
-    }
+    const [menuVisible, setMenuVisible] = useState(false);
+    const toggleMenuVisible = () => setMenuVisible(!menuVisible);
+
+    const menuClass = menuVisible ? headerMenuVisible : headerMenuHidden;
 
     return (
         <div className={header}>
@@ -36,13 +32,13 @@ const Header = () => {
                 <button
                     id='button'
                     className="flex items-center px-3 py-2 mr-10 border rounded text-white border-white hover:text-black hover:border-black"
-                    onClick={() => onClickShowMenu() }
+                    onClick={toggleMenuVisible}
                 >
                     <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
                 </button>
             </div>
             
-            <div id='menu' className={headerMenu}>
+            <div className={menuClass}>
                 {links.map((link) => (
                     <Link key={link.text} to={link.url} my={4} mx={8}>
                         <a className={location.pathname === link.url
--- src/views/components/layout/Styles.js
+++ src/views/components/layout/Styles.js
@@ -1,9 +1,10 @@
 export const styles = {
-    header: `flex flex-wrap px-4 py-4 bg-red-600`,
-    headerLinkActive: `text-white text-sm font-bold px-10 underline block`,
-    headerLinkInactive: `text-white text-sm font-bold px-10 block`,
+    header: `flex flex-wrap px-4 py-5 bg-red-600`,
+    headerLinkActive: `text-white text-sm font-bold px-10 underline block hover:text-black`,
+    headerLinkInactive: `text-white text-sm font-bold px-10 block hover:text-black`,
 
-    headerMenu: `w-full block flex-grow lg:flex lg:items-center lg:w-auto text-center`,
+    headerMenuVisible: `w-full block flex-grow lg:flex lg:items-center lg:w-auto text-center lg:flex-wrap`,
+    headerMenuHidden: `w-full block flex-grow lg:flex lg:items-center lg:w-auto text-center lg:flex-wrap hidden`,
 
     footer: `grid grid-cols-1 text-center py-4 bg-red-600`,
     textFooter: `text-white text-sm`
